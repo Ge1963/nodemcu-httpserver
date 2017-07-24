78aa499f238b8a93291774abdc1ef2b2535a2afa
Gerhard Schwaerzler <gerhard@schwaerzler.at>
2017-07-24 20:10:47 +0200

    LED testpage

---
+++ http/led.lua
@@ -0,0 +1,39 @@
+return function (connection, req, args)
+   dofile("httpserver-header.lc")(connection, 200, 'html')
+   connection:send([===[
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <title>NodeMCU LED controll</title>
+</head>
+<body>
+   <h1>NodeMCU LED controll</h1>
+]===])
+
+   if req.method == "GET" then
+      connection:send([===[
+      <form method="POST">
+         LED-1:<br/>
+         <input type="number" name="led-1"><br/>
+         <input type="submit" name="submit" value="Submit">
+      </form>
+      ]===])
+   elseif req.method == "POST" then
+      local rd = req.getRequestData()
+      connection:send('<h2>Received the following values:</h2>')
+      connection:send("<ul>\n")
+      for name, value in pairs(rd) do
+          connection:send('<li><b>')
+          connection:send(name)
+          connection:send(':</b> ')
+          connection:send(tostring(value))
+          connection:send("<br></li>\n")
+      end
+      connection:send("</ul>\n")
+   else
+      connection:send("ERROR WTF req.method is ", req.method)
+   end
+
+   connection:send('</body></html>')
+end
