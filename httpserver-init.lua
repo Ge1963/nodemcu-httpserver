7b1d411f7ccfc8970330f00346962b574f35d14d
Gerhard Schwaerzler <gerhard@schwaerzler.at>
2017-08-08 21:20:17 +0200

    key-value table access with "." selector

--- httpserver-init.lua
+++ httpserver-init.lua
@@ -6,12 +6,12 @@
 -- If you compiled the mdns module, then it will also register with mDNS.
 local startServer = function(ip)
    local conf = dofile('httpserver-conf.lc')
-   if (dofile("httpserver.lc")(conf['general']['port'])) then
+   if (dofile("httpserver.lc")(conf.general.port)) then
       print("nodemcu-httpserver running at:")
-      print("   http://" .. ip .. ":" ..  conf['general']['port'])
+      print("   http://" .. ip .. ":" ..  conf.general.port)
       if (mdns) then
-         mdns.register(conf['mdns']['hostname'], { description=conf['mdns']['description'], service="http", port=conf['general']['port'], location=conf['mdns']['location'] })
-         print ('   http://' .. conf['mdns']['hostname'] .. '.local.:' .. conf['general']['port'])
+         mdns.register(conf.mdns.hostname, { description=conf.mdns.description, service="http", port=conf.general.port, location=conf.mdns.location })
+         print ('   http://' .. conf.mdns.hostname .. '.local.:' .. conf.general.port)
       end
    end
    conf = nil
@@ -22,8 +22,8 @@ if (wifi.getmode() == wifi.STATION) or (wifi.getmode() == wifi.STATIONAP) then
    -- Connect to the WiFi access point and start server once connected.
    -- If the server loses connectivity, server will restart.
    wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(args)
-      print("Connected to WiFi Access Point. Got IP: " .. args["IP"])
-      startServer(args["IP"])
+      print("Connected to WiFi Access Point. Got IP: " .. args.IP)
+      startServer(args.IP)
       wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(args)
          print("Lost connectivity! Restarting...")
          node.restart()
